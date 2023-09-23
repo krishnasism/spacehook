d38c6fd2b49fe8686a50c3dabf050adcf4beb391
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2023-09-24 00:15:06 +0200

    Some ui

--- frontend/src/components/AddRequest.vue
+++ frontend/src/components/AddRequest.vue
@@ -7,11 +7,11 @@
                     <div class="sm:col-span-2">
                         <label for="name"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Endpoint</label>
-                            <div class="flex">
-                        <span class="p-2.5">/hook/</span>
-                        <input type="text" name="endpoint" id="endpoint" v-model="endpoint"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                            placeholder="test" required>
+                        <div class="flex">
+                            <span class="text-xs p-3 pl-0">/api/hook/</span>
+                            <input type="text" name="endpoint" id="endpoint" v-model="endpoint"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="test" required>
                         </div>
                     </div>
                     <div class="w-full">
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="w-full">
+    <div id="alert-1" v-if="showTopTip"
+      class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+      role="alert">
+      <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+        viewBox="0 0 20 20">
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+      </svg>
+      <span class="sr-only">Info</span>
+      <div class="ml-3 text-sm font-medium">
+        You can start calling the /api endpoint!
+      </div>
+      <button type="button" @click="showTopTip = false"
+        class="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+        data-dismiss-target="#alert-1" aria-label="Close">
+        <span class="sr-only">Close</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+      </button>
+    </div>
     <button @click="openAddRequest" type="button"
       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Add
       Request</button>
@@ -30,6 +52,7 @@ export default {
       requests: [],
       sortedRequest: [],
       showAddRequestModal: false,
+      showTopTip: true,
     }
   },
   setup() {
