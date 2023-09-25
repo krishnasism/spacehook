447473c12f76347b63da658ad566d05ce08afefb
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2023-09-25 18:19:50 +0200

    Make ui like space

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
   <div>
-    <RouterView class="p-4 bg-white self-start mt-20" />
+    <RouterView class="p-4 self-start mt-20" />
   </div>
 </template>
 
