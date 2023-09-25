e66323006a2108cdf7e3c61f8c552cbf130809af
Krishnasis Mandal <krishnasis.mandal@prestatech.com>
2023-09-26 00:09:48 +0200

    UI changes

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -22,34 +22,13 @@
         </svg>
       </button>
     </div>
-    <button @click="openAddRequest" type="button"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 mr-2 mb-2">
-      <span class="flex">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-          <path
-            d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-        </svg>
-        <span class="mr-2">Hook</span>
-      </span>
-    </button>
-    <button @click="showAllHooks" type="button"
-      class="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2.5 mr-2 mb-2">
-      <span class="flex">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          class="w-5 h-5 mr-16">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <span class="mr-2">My Hooks</span>
-      </span>
-    </button>
     <transition name="modal-fade" mode="out-in">
       <div class="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50" v-if="showRequest">
         <div class="bg-white rounded-lg shadow-lg p-4"
           style="max-height: 80vh; max-width: 100vh; overflow-y: auto; position: relative;">
           <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700" @click="showRequest = false">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-              class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
@@ -58,34 +37,83 @@
       </div>
     </transition>
     <transition name="modal-fade" mode="out-in">
-    <div class="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50" v-if="showHooksModal">
-      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 p-8"
-        style="max-height: 80vh; max-width: 100vh; overflow-y: auto;">
-        <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700" @click="showHooksModal = false">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-        <HooksList :hooks="hooks" v-if="hooks != null && showHooksModal" @hook-delete="deleteHook" :hooksLoading="hooksLoading"></HooksList>
+      <div class="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50" v-if="showHooksModal">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 p-8"
+          style="max-height: 80vh; max-width: 100vh; overflow-y: auto;">
+          <button class="absolute top-2 right-2 text-gray-500 hover:text-gray-700" @click="showHooksModal = false">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+          <HooksList :hooks="hooks" v-if="hooks != null && showHooksModal" @hook-delete="deleteHook"
+            :hooksLoading="hooksLoading"></HooksList>
+        </div>
       </div>
-    </div>
-  </transition>
-    <transition name="modal-fade" mode="out-in">
-    <div class="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50" v-if="showAddRequestModal">
-      <div class="bg-white rounded-lg shadow-lg p-4">
-        <AddRequest @close="showAddRequestModal = false" @submit="submitNewRequest"></AddRequest>
-      </div>
-    </div>
     </transition>
-    <div v-if="loading"><LoadingCircle/></div>
-    <RequestsList v-else :requests="requests" @request-clicked="showRequestModal" @request-delete="deleteRequest"></RequestsList>
+    <transition name="modal-fade" mode="out-in">
+      <div class="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50" v-if="showAddRequestModal">
+        <div class="bg-white rounded-lg shadow-lg p-4">
+          <AddRequest @close="showAddRequestModal = false" @submit="submitNewRequest"></AddRequest>
+        </div>
+      </div>
+    </transition>
+    <div v-if="loading">
+      <LoadingCircle />
+    </div>
+    <RequestsList v-else :requests="requests" @request-clicked="showRequestModal" @request-delete="deleteRequest">
+    </RequestsList>
     <Transition enter-active-class="duration-300 ease-out" enter-from-class="transform opacity-0"
       enter-to-class="opacity-100" leave-active-class="duration-200 ease-in" leave-from-class="opacity-100"
       leave-to-class="transform opacity-0">
-      <FailureToast class="z-20" style="position: fixed; bottom: 0; right: 20;" v-if="showFailureToast" :message="failureToastMessage"
-        @close-toast="showFailureToast = false"></FailureToast>
+      <FailureToast class="z-20" style="position: fixed; bottom: 0; right: 20;" v-if="showFailureToast"
+        :message="failureToastMessage" @close-toast="showFailureToast = false"></FailureToast>
     </Transition>
+
+    <div data-dial-init class="fixed right-6 bottom-6 group">
+      <div id="speed-dial-menu-bottom-right" class="flex flex-col items-center hidden mb-4 space-y-2">
+        <button type="button" data-tooltip-target="tooltip-all-hooks" data-tooltip-placement="left" @click="showAllHooks"
+          class="flex justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200 dark:border-gray-600 dark:hover:text-white shadow-sm dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+          </svg>
+          <span class="sr-only">All Hooks</span>
+        </button>
+        <div id="tooltip-all-hooks" role="tooltip"
+          class="w-auto absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+          All Hooks
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+
+        <button type="button" data-tooltip-target="tooltip-add-hook" data-tooltip-placement="left" @click="openAddRequest"
+          class="flex justify-center items-center w-[52px] h-[52px] text-gray-500 hover:text-gray-900 bg-white rounded-full border border-gray-200 dark:border-gray-600 dark:hover:text-white shadow-sm dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+
+          <span class="sr-only">New Hook</span>
+        </button>
+        <div id="tooltip-add-hook" role="tooltip"
+        class="w-auto absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+          New Hook
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      </div>
+      <button type="button" data-dial-toggle="speed-dial-menu-bottom-right" aria-controls="speed-dial-menu-bottom-right"
+        aria-expanded="false"
+        class="flex items-center justify-center text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+        <svg class="w-5 h-5 transition-transform group-hover:rotate-45" aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M9 1v16M1 9h16" />
+        </svg>
+        <span class="sr-only">Open actions menu</span>
+      </button>
+    </div>
+
   </div>
 </template>
 
@@ -130,7 +158,7 @@ export default {
     this.loading = false;
     document.addEventListener("keydown", this.handleKeyDown);
   },
-  beforeUnmount(){
+  beforeUnmount() {
     document.removeEventListener("keydown", this.handleKeyDown);
   },
   computed: {
@@ -265,7 +293,13 @@ export default {
   transition: opacity 0.5s;
 }
 
-.modal-fade-enter, .modal-fade-leave-to {
+.modal-fade-enter,
+.modal-fade-leave-to {
   opacity: 0;
+}
+
+.tooltip {
+  white-space: nowrap;
+  display: inline-block;
 }
 </style>
